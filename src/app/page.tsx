3588949a44b66d48b5e41bce0bf768512a26c74b
Andrew Marfo <andrewmarfo23@gmail.com>
2024-07-17 16:59:13 +0000

    markdown and preview files imported in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "@/components/navbar";
+import Markdown from "@/components/markdown";
+import Preview from "@/components/preview";
 
 export default function Home() {
   return (
     <div className="home">
+      {/* navbar */}
       <Navbar />
+      <div>
+        <Markdown/>
+        <Preview/>
+      </div>
     </div>
   );
 }
