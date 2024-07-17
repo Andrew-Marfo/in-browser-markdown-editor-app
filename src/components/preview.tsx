746d3f7e4a78db6c9030992526098aa0028e32f5
Andrew Marfo <andrewmarfo23@gmail.com>
2024-07-17 19:29:13 +0000

    passed markdownText to preview.tsx for markdown display

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import HeroHeader from './hero_header'
-import Image from 'next/image'
+import React from "react";
+import HeroHeader from "./hero_header";
+import Image from "next/image";
+import { marked } from "marked";
+import dompurify from 'dompurify';
 
-const  Preview=() =>{
-  return (
-    <div className='preview'>
-        <HeroHeader>
-            <p className='hero-header-title'>preview</p>
-            <Image src={"/assets/icon-show-preview.svg"} alt='show preview' width={13} height={10}/>
-        </HeroHeader>
-    </div>
-  )
+interface PreviewProps {
+  markdownText: string;
 }
 
-export default Preview
+const Preview = ({ markdownText }: PreviewProps) => {
+  const parsed = dompurify.sanitize(marked.parse(markdownText))
+//   const parsed = marked.parse(markdownText);
+  return (
+    <div className="preview">
+      <HeroHeader>
+        <p className="hero-header-title">preview</p>
+        <Image
+          src={"/assets/icon-show-preview.svg"}
+          alt="show preview"
+          width={13}
+          height={10}
+        />
+      </HeroHeader>
+
+      {/* <textarea className="markdown-textarea" disabled></textarea> */}
+      {/* {parsed} */}
+      <div className="preview-container">
+        <div dangerouslySetInnerHTML={{ __html: parsed }} className="preview-items"/>
+      </div>
+    </div>
+  );
+};
+
+export default Preview;
