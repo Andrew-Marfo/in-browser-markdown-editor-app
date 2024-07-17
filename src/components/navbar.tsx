b8d9ff8055a03535d46dba5a1839deac70222d2a
Andrew Marfo <andrewmarfo23@gmail.com>
2024-07-17 14:16:45 +0000

    Designing the nav bar(colors and font sizes)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,9 +6,10 @@ const Navbar = () => {
     <div className='navbar flex items-center'>
       <div className='nav-details flex items-center'>
         <button className='hamburger'>
-          <Image src="/assets/icon-menu.svg" alt='hamburger' width={20} height={10}/>
+          <Image src="/assets/icon-menu.svg" alt='hamburger' width={20} height={13}/>
         </button>
-        <p>markdown</p>
+        <p className='markdown-title'>markdown</p>
+        <p className='separator'>|</p>
         <div className='nav-doc-name flex items-center'>
           <div>
             <Image src="/assets/icon-document.svg" alt='link' width={10} height={13}/>
@@ -22,11 +23,11 @@ const Navbar = () => {
       
       <div className='nav-action-buttons flex items-center'>
         <button className='delete-btn'>
-        <Image src="/assets/icon-delete.svg" alt='link' width={10} height={13}/>
+        <Image src="/assets/icon-delete.svg" alt='link' width={13} height={13}/>
         </button>
         <button className='save-btn flex items-center'>
         <Image src="/assets/icon-save.svg" alt='link' width={10} height={13}/>
-           save</button>
+           Save Changes</button>
       </div>
     </div>
   )
