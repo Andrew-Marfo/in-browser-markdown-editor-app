734b2f01de1800358a63ccbe8ceb6fcf51eaf0f3
Andrew Marfo <andrewmarfo23@gmail.com>
2024-07-17 19:35:34 +0000

    removed dompurify from preview.tsx

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import HeroHeader from "./hero_header";
 import Image from "next/image";
 import { marked } from "marked";
-import dompurify from 'dompurify';
 
 interface PreviewProps {
   markdownText: string;
 }
 
 const Preview = ({ markdownText }: PreviewProps) => {
-  const parsed = dompurify.sanitize(marked.parse(markdownText))
-//   const parsed = marked.parse(markdownText);
+  const parsed = marked.parse(markdownText);
+  //   const parsed = marked.parse(markdownText);
   return (
     <div className="preview">
       <HeroHeader>
@@ -26,7 +25,10 @@ const Preview = ({ markdownText }: PreviewProps) => {
       {/* <textarea className="markdown-textarea" disabled></textarea> */}
       {/* {parsed} */}
       <div className="preview-container">
-        <div dangerouslySetInnerHTML={{ __html: parsed }} className="preview-items"/>
+        <div
+          dangerouslySetInnerHTML={{ __html: parsed }}
+          className="preview-items"
+        />
       </div>
     </div>
   );
