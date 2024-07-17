9a111662cdfeb36c0a87c7ddd9869311883a71c9
Andrew Marfo <andrewmarfo23@gmail.com>
2024-07-17 19:26:50 +0000

    created state variable: markdownText

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "@/components/navbar";
 import Markdown from "@/components/markdown";
 import Preview from "@/components/preview";
+import { useState } from "react";
 
 export default function Home() {
+  const [markdownText, setMarkdownText] = useState("")
+
+  // const handleMarkdownText = (value) => {
+  //   setMarkdownText(value)
+  // }
+  console.log(markdownText)
+
   return (
     <div className="home">
       {/* navbar */}
       <Navbar />
       <div className="hero flex justify-between">
-        <Markdown/>
-        <Preview/>
+        <Markdown markdownText={markdownText} setMarkdownText={setMarkdownText}/>
+        <Preview markdownText={markdownText}/>
       </div>
     </div>
   );
