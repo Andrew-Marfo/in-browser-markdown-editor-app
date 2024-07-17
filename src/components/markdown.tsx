3f49dea8c83f96786f6f7baf6b6aaa30be5aad6e
Andrew Marfo <andrewmarfo23@gmail.com>
2024-07-17 19:25:05 +0000

    passed props to markdown.tsx to update markdownText

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import HeroHeader from "./hero_header";
 
-const Markdown = () => {
-  return <div className="markdown">
-    <HeroHeader>
-      <p className="hero-header-title">markdown</p>
-    </HeroHeader>
-  </div>;
+interface MarkdownProps {
+  markdownText: string;
+  setMarkdownText: (value: string) => void;
+}
+
+const Markdown = ({markdownText, setMarkdownText} : MarkdownProps) => {
+  return (
+    <div className="markdown">
+      <HeroHeader>
+        <p className="hero-header-title">markdown</p>
+      </HeroHeader>
+
+      <textarea className="markdown-textarea" onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMarkdownText(e.target.value)}>{markdownText}</textarea>
+    </div>
+  );
 };
 
 export default Markdown;
