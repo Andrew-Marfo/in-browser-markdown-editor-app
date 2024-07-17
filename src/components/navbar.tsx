128b23e97a152638c953e469e54e9dc9f125061a
Andrew Marfo <andrewmarfo23@gmail.com>
2024-07-17 16:39:55 +0000

    completed nav bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,18 +5,20 @@ const Navbar = () => {
   return (
     <div className='navbar flex items-center'>
       <div className='nav-details flex items-center'>
+        {/* humburger */}
         <button className='hamburger'>
           <Image src="/assets/icon-menu.svg" alt='hamburger' width={20} height={13}/>
         </button>
         <p className='markdown-title'>markdown</p>
         <p className='separator'>|</p>
+        {/* document name details */}
         <div className='nav-doc-name flex items-center'>
           <div>
             <Image src="/assets/icon-document.svg" alt='link' width={10} height={13}/>
           </div>
           <div>
-            <p>doc name</p>
-            <p>name</p>
+            <p className='doc-name-title'>Document Name</p>
+            <p className='doc-name'>welcome.md</p>
           </div>
         </div>
       </div>
@@ -26,8 +28,9 @@ const Navbar = () => {
         <Image src="/assets/icon-delete.svg" alt='link' width={13} height={13}/>
         </button>
         <button className='save-btn flex items-center'>
-        <Image src="/assets/icon-save.svg" alt='link' width={10} height={13}/>
-           Save Changes</button>
+          <Image src="/assets/icon-save.svg" alt='link' width={13} height={13}/>
+           Save Changes
+        </button>
       </div>
     </div>
   )
